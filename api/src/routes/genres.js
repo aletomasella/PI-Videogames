7721cd3fc57414746c5cfac77d3635236f9456e5
Alejandro Tomasella <aletomasella09@gmail.com>
2022-03-26 22:59:41 -0300

    Segundo Commit

--- api/src/routes/genres.js
+++ api/src/routes/genres.js
@@ -1,7 +1,24 @@
+const { default: axios } = require("axios");
 const { Router } = require("express");
+const { apiKey } = require("../db");
+const { Genre } = require("../db");
 
 const router = Router();
 
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`https://api.rawg.io/api/genres${apiKey}`);
+    const genres = response.data.results;
+    const genresNames = genres.map((genre) => genre.name);
+    await genresNames.forEach((name) => {
+      Genre.create({
+        name,
+      });
+    });
+    res.json(genresNames);
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 module.exports = router;
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { apiKey } = require("../db.js");
 const axios = require("axios");
+const { Videogame } = require("../db");
 
 const router = Router();
 
@@ -11,9 +12,30 @@ router.get("/", (req, res, next) => {
 
 router.get("/", async (req, res) => {
   const response = await axios.get(`https://api.rawg.io/api/games${apiKey}`);
-  const games = response.data;
-  // console.log(games);
-  res.json(games.results);
+  const games = response.data.results;
+  const gamesData = games.map((game) => {
+    const description = game.tags.map((tag) => tag.name);
+    const platforms = game.platforms.map((plat) => plat.platform.name);
+    return {
+      name: game.name,
+      description: description.join(" "),
+      launchDate: game.released,
+      rating: game.rating,
+      platforms: platforms.join(" "),
+    };
+  });
+  // Cargo los datos traidos y filtrados de la API a mi DB. Me falta configurar el tema de los generos.
+  await gamesData.forEach((game) => {
+    Videogame.create({
+      name: game.name,
+      description: game.description,
+      launchDate: game.launchDate,
+      rating: game.rating,
+      platforms: game.platforms,
+    });
+  });
+
+  res.json(gamesData);
 });
 
 router.get("/:id", (req, res) => {});
