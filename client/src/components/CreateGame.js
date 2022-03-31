ee7c75ed173f9945043b42ca2b42f09a87b03e08
Alejandro Tomasella <aletomasella09@gmail.com>
2022-03-31 10:39:23 -0300

    Front Final v1.0.0

--- client/src/components/CreateGame.js
+++ client/src/components/CreateGame.js
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { createVideogame, getAllGenres } from "../action";
+import {
+  conteiner,
+  inputs,
+  checkboxsConteiner,
+  checkmark,
+  botonSubmit,
+  inputDescription,
+  danger,
+  selectMenu,
+} from "./CreateGame.module.css";
 
 function validate(input) {
   const errors = {};
@@ -28,7 +38,13 @@ function CreateGame() {
     genres: [],
     platforms: "",
   });
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+    rating: "",
+    platforms: "",
+    genres: [],
+  });
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -104,176 +120,206 @@ function CreateGame() {
   };
   return (
     <>
-      <Link to="/home">Volver a Home</Link>
-      <h1>Formulacion de Creacion</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nombre</label>
-          <input
-            type="text"
-            name="name"
-            value={input.name}
-            onChange={handleChange}
-          />
-          {errors.name && <label>{errors.name}</label>}
-        </div>
-        <div>
-          <label htmlFor="description">Descripcion</label>
-          <input
-            type="text"
-            name="description"
-            value={input.description}
-            onChange={handleChange}
-          />
-          {errors.description && <label>{errors.description}</label>}
-        </div>
-        <div>
-          <label htmlFor="launchDate">Fecha de Lanzamiento</label>
-          <input
-            type="date"
-            name="launchDate"
-            value={input.launchDate}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="rating">Rating</label>
-          <input
-            type="number"
-            name="rating"
-            value={input.rating}
-            onChange={handleChange}
-          />
-          {errors.rating && <label>{errors.rating}</label>}
-        </div>
-        <div>
-          <label>Plataformas: </label>
-          <label htmlFor="Xbox Series">
+      <div className={conteiner}>
+        <Link to="/home">Volver a Home</Link>
+        <h1>Formulacion de Creacion</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Nombre</label>
             <input
-              type="checkbox"
-              value="Xbox Series"
-              name="Xbox Series"
-              onChange={handleChecked}
+              type="text"
+              name="name"
+              value={input.name}
+              onChange={handleChange}
+              className={inputs}
             />
-            Xbox Series
-          </label>
-          <label htmlFor="Playstation 4">
+            {errors.name && <label className={danger}>{errors.name}</label>}
+          </div>
+          <div>
+            <label htmlFor="description">Descripcion</label>
+            {/* <input
+              type="text"
+              name="description"
+              value={input.description}
+              onChange={handleChange}
+              className={inputDescription}
+            /> */}
+            <textarea
+              name="description"
+              value={input.description}
+              onChange={handleChange}
+              className={inputDescription}
+            />
+            {errors.description && (
+              <label className={danger}>{errors.description}</label>
+            )}
+          </div>
+          <div>
+            <label htmlFor="launchDate">Fecha de Lanzamiento</label>
             <input
-              type="checkbox"
-              value="Playstation 4"
-              name="Playstation 4"
-              onChange={handleChecked}
+              type="date"
+              name="launchDate"
+              value={input.launchDate}
+              onChange={handleChange}
+              className={inputs}
             />
-            Playstation 4
-          </label>
-          <label htmlFor="PC">
+          </div>
+          <div>
+            <label htmlFor="rating">Rating</label>
             <input
-              type="checkbox"
-              value="PC"
-              name="PC"
-              onChange={handleChecked}
+              type="number"
+              name="rating"
+              value={input.rating}
+              onChange={handleChange}
+              className={inputs}
             />
-            PC
-          </label>
-          <label htmlFor="Android">
-            <input
-              type="checkbox"
-              value="Android"
-              name="Android"
-              onChange={handleChecked}
-            />
-            Android
-          </label>
-          <label htmlFor="Nintento Switch">
-            <input
-              type="checkbox"
-              value="Nintento Switch"
-              name="Nintento Switch"
-            />
-            Nintento Switch
-          </label>
-          <label htmlFor="IOS">
-            <input
-              type="checkbox"
-              value="IOS"
-              name="IOS"
-              onChange={handleChecked}
-            />
-            IOS
-          </label>
-          <label htmlFor="Playstation 5">
-            <input
-              type="checkbox"
-              value="Playstation 5"
-              name="Playstation 5"
-              onChange={handleChecked}
-            />
-            Playstation 5
-          </label>
-          <label htmlFor="Xbox One">
-            <input
-              type="checkbox"
-              value="Xbox One"
-              name="Xbox One"
-              onChange={handleChecked}
-            />
-            Xbox One
-          </label>
-          <label htmlFor="Linux">
-            <input
-              type="checkbox"
-              value="Linux"
-              name="Linux"
-              onChange={handleChecked}
-            />
-            Linux
-          </label>
-          <label htmlFor="macOS">
-            <input
-              type="checkbox"
-              value="macOS"
-              name="macOS"
-              onChange={handleChecked}
-            />
-            macOS
-          </label>
-          <label htmlFor="Playstation 3">
-            <input
-              type="checkbox"
-              value="Playstation 3"
-              name="Playstation 3"
-              onChange={handleChecked}
-            />
-            Playstation 3
-          </label>
-          <label htmlFor="Xbox 360">
-            <input
-              type="checkbox"
-              value="Xbox 360"
-              name="Xbox 360"
-              onChange={handleChecked}
-            />
-            Xbox 360
-          </label>
-          {errors.platforms && <p>{errors.platforms}</p>}
-        </div>
-        <select name="Generos" onChange={handleGenres}>
-          {genres &&
-            genres.map((genre) => {
-              return (
-                <option value={genre.name} key={genre.id}>
-                  {genre.name}
-                </option>
-              );
-            })}
-        </select>
-        {input.genres.length ? (
-          <p>Generos seleccionados: {input.genres.join(" ")}</p>
-        ) : null}
-        {errors.genres && <p>{errors.genres}</p>}
-        <br />
-        <input type="submit" />
-      </form>
+            {errors.rating && <label className={danger}>{errors.rating}</label>}
+          </div>
+          <div>
+            <label>Plataformas: </label>
+            <label htmlFor="Xbox Series" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Xbox Series"
+                name="Xbox Series"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Xbox Series
+            </label>
+            <label htmlFor="Playstation 4" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Playstation 4"
+                name="Playstation 4"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Playstation 4
+            </label>
+            <label htmlFor="PC" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="PC"
+                name="PC"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              PC
+            </label>
+            <label htmlFor="Android" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Android"
+                name="Android"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Android
+            </label>
+            <label htmlFor="Nintento Switch" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Nintento Switch"
+                name="Nintento Switch"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Nintento Switch
+            </label>
+            <label htmlFor="IOS" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="IOS"
+                name="IOS"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              IOS
+            </label>
+            <label htmlFor="Playstation 5" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Playstation 5"
+                name="Playstation 5"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Playstation 5
+            </label>
+            <label htmlFor="Xbox One" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Xbox One"
+                name="Xbox One"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Xbox One
+            </label>
+            <label htmlFor="Linux" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Linux"
+                name="Linux"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Linux
+            </label>
+            <label htmlFor="macOS" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="macOS"
+                name="macOS"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              macOS
+            </label>
+            <label htmlFor="Playstation 3" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Playstation 3"
+                name="Playstation 3"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Playstation 3
+            </label>
+            <label htmlFor="Xbox 360" className={checkboxsConteiner}>
+              <input
+                type="checkbox"
+                value="Xbox 360"
+                name="Xbox 360"
+                onChange={handleChecked}
+                className={checkmark}
+              />
+              Xbox 360
+            </label>
+            <br />
+            {errors.platforms && (
+              <label className={danger}>{errors.platforms}</label>
+            )}
+          </div>
+          <select name="Generos" onChange={handleGenres} className={selectMenu}>
+            {genres &&
+              genres.map((genre) => {
+                return (
+                  <option value={genre.name} key={genre.id}>
+                    {genre.name}
+                  </option>
+                );
+              })}
+          </select>
+          {input.genres.length ? (
+            <p>Generos seleccionados: {input.genres.join(" ")}</p>
+          ) : null}
+          {errors.genres && <label className={danger}>{errors.genres}</label>}
+          <br />
+          <input type="submit" className={botonSubmit} />
+        </form>
+      </div>
     </>
   );
 }
