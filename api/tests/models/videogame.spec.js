9032fb4e6d8c7ddf53f70014deeef665917e635a
Alejandro Tomasella <aletomasella09@gmail.com>
2022-04-11 12:33:31 -0300

    DB Back testing

--- api/tests/models/videogame.spec.js
+++ api/tests/models/videogame.spec.js
@@ -1,22 +1,68 @@
-const { Videogame, conn } = require('../../src/db.js');
-const { expect } = require('chai');
+const { Videogame, conn, videogameModel } = require("../../src/db.js");
+const { expect } = require("chai");
 
-describe('Videogame model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    }));
-  describe('Validators', () => {
-    beforeEach(() => Videogame.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Videogame.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
+describe("Modelo de Videojuegos", () => {
+  const game = {
+    name: "Mario",
+    description: "Este juego es una masa, pero tiene unos años ya",
+    platforms: "PC XBOX PS NINTENTO",
+    launchDate: "03-27-1965",
+    rating: "4.5",
+  };
+  beforeEach(() => {
+    conn
+      //Aca se comprueba que se puede conectar a la base de datos.
+      .authenticate()
+      .catch((err) => {
+        console.error("No se puedo conectar a la base de datos : ", err);
       });
-      it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Super Mario Bros' });
+  });
+
+  describe("Validaciones", () => {
+    beforeEach(() => Videogame.sync({ force: true }));
+    describe("Nombre", () => {
+      it("Debe lanzar un error si el nombre esta vacio", () => {
+        return Videogame.create({
+          description: "Este juego es una masa, pero tiene unos años ya",
+          platforms: "PC XBOX PS NINTENTO",
+          launchDate: "03-27-1965",
+          rating: "4.5",
+        }).catch((err) => expect(err.errors[0].path).to.equal("name")); //Aca estoy accediendo dentro del error, al nombre de cual fue el atributo que genero el error.
+      });
+    });
+
+    describe("Descripcion", () => {
+      it("Debe lanzar un error si la descripcion esta vacia", () => {
+        return Videogame.create({
+          name: "Mario",
+          platforms: "PC XBOX PS NINTENTO",
+          launchDate: "03-27-1965",
+          rating: "4.5",
+        }).catch((err) => expect(err.errors[0].path).to.equal("description"));
+      });
+    });
+
+    describe("Plataformas", () => {
+      it("Debe lanzar un error si no se le asigno ninguna plataforma", () => {
+        Videogame.create({
+          name: "Mario",
+          description: "Este juego es una masa, pero tiene unos años ya",
+          launchDate: "03-27-1965",
+          rating: "4.5",
+        }).catch(() => expect(err.error[0].path.to.equal("platforms")));
       });
     });
   });
+  //   describe("Asosociaciones", () => {
+  //     it("Debe estar asociado correctamente con la tabla Genres", async () => {
+  //       const model = await Videogame.create({
+  //         name: "Mario",
+  //         description: "Este juego es una masa, pero tiene unos años ya",
+  //         launchDate: "03-27-1965",
+  //         rating: "4.5",
+  //         genres: ["RPG", "Action"],
+  //       });
+  //       expect(model.genreId).to.equal(seed.ca);
+  //     });
+  //   });
 });
--- api/tests/routes/videogame.spec.js
+++ api/tests/routes/videogame.spec.js
@@ -1,24 +1,42 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Videogame, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Videogame, conn } = require("../../src/db.js");
 
-const agent = session(app);
+const agent = session(app); //Aca le estoy pasando mi app de express a supertest y lo que me devuelve lo guardo en agent que me va permitir hacer los pedidos a la api.
 const videogame = {
-  name: 'Super Mario Bros',
+  name: "Mario",
+  description: "Este juego es una masa, pero tiene unos años ya",
+  platforms: "PC XBOX PS NINTENTO",
+  launchDate: "03-27-1965",
+  rating: "4.5",
 };
 
-describe('Videogame routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Videogame.sync({ force: true })
-    .then(() => Videogame.create(videogame)));
-  describe('GET /videogames', () => {
-    it('should get 200', () =>
-      agent.get('/videogames').expect(200)
-    );
+describe("Videogame routes", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  describe("GET /videogames", () => {
+    it("Debe devolver un 200", () => agent.get("/videogames").expect(200));
+    it("Debe devolver un array con los juegos", () =>
+      agent
+        .get("/videogames")
+        .then((response) => expect(response.body.length).to.equal(100)));
+  });
+  describe("POST /videogames", () => {
+    it("Debe devolver un error si no se pasan los parametros necesarios", () => {
+      agent
+        .post("/videogames")
+        .send({
+          description: "Este juego es una masa, pero tiene unos años ya",
+          platforms: "PC XBOX PS NINTENTO",
+          launchDate: "03-27-1965",
+          rating: "4.5",
+        })
+        .expect(404);
+    });
   });
 });
