b8d0a23b02661196f6f0c9baf3f0f0ceccbccda8
Alejandro Tomasella <aletomasella09@gmail.com>
2022-06-27 18:47:16 -0300

    Port Changes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const PORT = 443;
+const PORT = 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(process.env.PORT || PORT, () => {
     console.log("%s listening at " + PORT); // eslint-disable-line no-console
   });
 });
